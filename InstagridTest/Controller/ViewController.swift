59f5b4030b6ec46c960c1fb376caacf2e7aca028
Pyero <pierre.rgt@gmail.com>
2022-09-09 03:59:03 -0400

    no message

--- InstagridTest/Controller/ViewController.swift
+++ InstagridTest/Controller/ViewController.swift
@@ -2,11 +2,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+//MARK: Properties & outlets
+
+    //list of frame
+    enum whitchSquare: Int {
+    case topLeft
+    case topRight
+    case bottomLeft
+    case bottomRigth
+    
     }
+    
+    //list of layout
+    enum layoutFormat {
+    case rectangleTop
+        
+    }
+    
+@IBOutlet var buttonSquareTopLeft: UIButton?
+@IBOutlet var buttonSquareTopRigth: UIButton?
+@IBOutlet var buttonSquareBottomLeft: UIButton?
+@IBOutlet var buttonSquareBottomRigth: UIButton?
+    
+    //Buttons
+  @IBOutlet  var buttonLayoutRectangleTop: UIButton?
+  @IBOutlet  var buttonLayoutRectangleBottom: UIButton?
+  @IBOutlet  var buttonLayoutSquares: UIButton?
+    
+    
+    //Create an image of the global : To be sent after
+    
+    
 
-
+    
+    
+    //MARK: View life cycle
 }
 
