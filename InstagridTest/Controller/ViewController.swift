f2d14867cbb7013e4a473259d90233722f21d691
Pyero <pierre.rgt@gmail.com>
2022-09-21 21:22:51 -0400

    no message

--- InstagridTest/Controller/ViewController.swift
+++ InstagridTest/Controller/ViewController.swift
@@ -1,41 +1,79 @@
 import UIKit
 
-class ViewController: UIViewController {
-
-//MARK: Properties & outlets
-
-    //list of frame
-    enum whitchSquare: Int {
-    case topLeft
-    case topRight
-    case bottomLeft
-    case bottomRigth
+final class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
+    private lazy var imagePicker: UIImagePickerController = {
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        return imagePicker
+    }()
+
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let image = info[.originalImage] as? UIImage {
+            print(image.description)
+            topRightButton.setImage(image, for: .normal)
+        }
+        imagePicker.dismiss(animated: true)
     }
     
+    var toto = true
+    //MARK: Properties & outlets
+    
+    @IBOutlet weak var topRightButton: UIButton!
+    @IBOutlet weak var bottomRightButton: UIButton!
+    @IBOutlet weak var topLeftButton: UIButton!
+    @IBOutlet weak var bottomLeftButton: UIButton!
+    //list of frame
+    /*
+     enum whitchSquare: Int {
+     case topLeft
+     case topRight
+     case bottomLeft
+     case bottomRigth
+     }
+     */
     //list of layout
     enum layoutFormat {
-    case rectangleTop
+        case rectangleTop
         
     }
     
-@IBOutlet var buttonSquareTopLeft: UIButton?
-@IBOutlet var buttonSquareTopRigth: UIButton?
-@IBOutlet var buttonSquareBottomLeft: UIButton?
-@IBOutlet var buttonSquareBottomRigth: UIButton?
-    
-    //Buttons
-  @IBOutlet  var buttonLayoutRectangleTop: UIButton?
-  @IBOutlet  var buttonLayoutRectangleBottom: UIButton?
-  @IBOutlet  var buttonLayoutSquares: UIButton?
-    
-    
-    //Create an image of the global : To be sent after
-    
-    
-
-    
-    
     //MARK: View life cycle
+    
+    
+    
+    // MARK: - Inputs
+    
+    @IBAction func didPressTopLeftButton(_ sender: Any) {
+        // Ouvrir la bibliotheque de photo
+        
+        openGallary()
+    }
+    
+    @IBAction func didPressTopRigthButton(_ sender: Any) {
+        openGallary()
+    }
+    
+    @IBAction func didPressLayoutOneButton(_ sender: Any) {
+        // Configurer l'affichage ou non des bouton de la grid
+        topRightButton.isHidden = true
+        bottomRightButton.isHidden = false
+    }
+    
+    @IBAction func didPressLayoutTwoButton(_ sender: Any) {
+        bottomRightButton.isHidden = true
+        topRightButton.isHidden = false
+    }
+    @IBAction func didPressLayoutThreeButton(_ sender: Any) {
+        topRightButton.isHidden = false
+        bottomRightButton.isHidden = false
+    }
+    
+    func openGallary() {
+        imagePicker.sourceType = UIImagePickerController.SourceType.photoLibrary
+        imagePicker.allowsEditing = true
+        //imagePicker.delegate = self
+        self.present(imagePicker, animated: true, completion: nil)
+    }
 }
 
