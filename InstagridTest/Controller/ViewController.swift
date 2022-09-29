1010f2c2e3436255cf3919570f9e3b327412deb2
Pyero <pierre.rgt@gmail.com>
2022-09-29 08:09:46 -0400

    no message

--- InstagridTest/Controller/ViewController.swift
+++ InstagridTest/Controller/ViewController.swift
@@ -1,56 +1,74 @@
 import UIKit
 
-final class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+final class ViewController: UIViewController {
     
-    private lazy var imagePicker: UIImagePickerController = {
-        let imagePicker = UIImagePickerController()
-        imagePicker.delegate = self
-        return imagePicker
-    }()
-
-    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-        if let image = info[.originalImage] as? UIImage {
-            print(image.description)
-            topRightButton.setImage(image, for: .normal)
-        }
-        imagePicker.dismiss(animated: true)
-    }
+    /*
+     reflex:
+     
+    1. Outlet Collection: UIButton en remplacement de chaque connexion (properties & outlets)
+     =>  @IBOutlet var buttonArray: [UIButton]!
+     
+     
+     2. suivre les boutons préssé / 4 boutons, 4 tag 0 à 3
+     => private var ButtonTag = Int()
+    chaque IBAction a son tag
+     
+     
+    3. dans return image picker
+ 
+}
+     
+     créer 2 lazy var de type swipegesture
+     1 pour top gesture
+     1 pour left gesture
+     
+     trouver comment ecouter la closure de la gesture
+     // gérer l'animation du swipe
+     a l'exec de la closure, faire une action (ouvrir le shareviewcontroller)
     
-    var toto = true
+    */
+    
     //MARK: Properties & outlets
     
     @IBOutlet weak var topRightButton: UIButton!
     @IBOutlet weak var bottomRightButton: UIButton!
     @IBOutlet weak var topLeftButton: UIButton!
     @IBOutlet weak var bottomLeftButton: UIButton!
-    //list of frame
-    /*
-     enum whitchSquare: Int {
-     case topLeft
-     case topRight
-     case bottomLeft
-     case bottomRigth
-     }
-     */
-    //list of layout
-    enum layoutFormat {
-        case rectangleTop
-        
+    
+    private lazy var imagePicker: UIImagePickerController = {
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        return imagePicker
+    }()
+    
+    private var ButtonTag = Int()
+    private enum ButtonType {
+        case topLeft
+        case topRight
+        case bottomLeft
+        case bottomRight
     }
+    private var selectedButton: ButtonType?
     
-    //MARK: View life cycle
-    
-    
-    
-    // MARK: - Inputs
-    
-    @IBAction func didPressTopLeftButton(_ sender: Any) {
+    @IBAction func didPressTopLeftButton(_ sender: UIButton) {
         // Ouvrir la bibliotheque de photo
-        
+    
+        ButtonTag = 0
+        selectedButton = .topLeft
         openGallary()
+    
     }
     
     @IBAction func didPressTopRigthButton(_ sender: Any) {
+        openGallary()
+        
+    }
+    
+    @IBAction func didPressBottomLeftButtom(_ sender: Any) {
+        openGallary()
+    }
+    
+    @IBAction func didPressBottomRightButton(_ sender: Any) {
         openGallary()
     }
     
@@ -69,11 +87,43 @@ final class ViewController: UIViewController, UIImagePickerControllerDelegate, U
         bottomRightButton.isHidden = false
     }
     
-    func openGallary() {
-        imagePicker.sourceType = UIImagePickerController.SourceType.photoLibrary
+  
+}
+
+// MARK: - UIImagePickerControllerDelegate, UINavigationControllerDelegate
+extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let image = info[.originalImage] as? UIImage {
+            set(image)
+        }
+        imagePicker.dismiss(animated: true)
+    }
+    
+    private func set(_ image: UIImage) {
+        let imageView = UIImageView(image: image)
+        switch selectedButton {
+        case .topLeft:
+            topLeftButton.removeAllSubviews()
+            imageView.frame = topLeftButton.bounds
+            topLeftButton.addSubview(imageView)
+        case .topRight: topRightButton.setImage(image, for: .normal)
+        case .bottomLeft: bottomRightButton.setImage(image, for: .normal)
+        case .bottomRight: bottomLeftButton.setImage(image, for: .normal)
+        default: break
+        }
+    }
+    
+    private func openGallary() {
+        imagePicker.sourceType = .photoLibrary
         imagePicker.allowsEditing = true
-        //imagePicker.delegate = self
         self.present(imagePicker, animated: true, completion: nil)
     }
 }
 
+extension UIView {
+    func removeAllSubviews() {
+        self.subviews.forEach { subView in
+            subView.removeFromSuperview()
+        }
+    }
+}
